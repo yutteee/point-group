322bef691759d3b0a11bd0cc4745b5eb64d6f064
yutteee <yusaku_nakamura_aa@icloud.com>
2022-04-26 16:19:23 +0900

    各コンポーネントのデザイン改良した

--- point-group/src/components/QuestionCard.vue
+++ point-group/src/components/QuestionCard.vue
@@ -1,16 +1,19 @@
 <template>
-    <h1 class="question">{{ mainQuestion }}</h1>
+    <h1 class="question">Q.{{ mainQuestion }}</h1>
 </template>
 
 <script>
 export default {
-    props: [
-        'mainQuestion']
+    props: ['mainQuestion']
 }
 </script>
 
 <style scoped>
 .question {
     border: thick double #54BAB9;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 64px;
 }
 </style>
